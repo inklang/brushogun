bdd247dae22604aa42d1f35ed41e734d9569b4d9
mintychochip <jlo2@csub.edu>
2026-03-20 21:58:00 -0700

    feat: add CompiledScript and InkCompiler stubs for bukkit module (cherry-pick of 5976643)

---
+++ ink/src/main/kotlin/org/inklang/CompiledScript.kt
@@ -0,0 +1,18 @@
+package org.inklang
+
+import org.inklang.lang.Chunk
+import org.inklang.lang.VM
+
+/**
+ * A compiled Ink script that can be executed with a context.
+ */
+class CompiledScript(private val chunk: Chunk) {
+    /**
+     * Execute this script with the given context.
+     */
+    fun execute(context: InkContext) {
+        val vm = VM()
+        // TODO: Wire up context (log, print, event handlers) to VM globals
+        vm.execute(chunk)
+    }
+}
